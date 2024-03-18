cefa1cd86be357c4340a68717b8cedc9945e8837
Alvaro <alujan8@alu.ucam.edu>
2024-03-18 09:34:11 +0100

    Getters y setters implementados

--- Empleado.java
+++ Empleado.java
@@ -17,4 +17,28 @@ class Empleado {
                 ", salario=" + salario +
                 '}';
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }
