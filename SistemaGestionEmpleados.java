4383f728e8726de1987b93483624ee6aa224a26c
Alvaro <alujan8@alu.ucam.edu>
2024-03-18 10:00:45 +0100

    Sistema de empleados corregida

--- SistemaGestionEmpleados.java
+++ SistemaGestionEmpleados.java
@@ -1,26 +1,3 @@
-public class SistemaGestionEmpleados {
-
-    private Empleado[] empleados;
-
-    public SistemaGestionEmpleados(Empleado[] empleados) {
-        this.empleados = empleados;
-    }
-
-    public void aumentarSalario(double porcentaje) {
-        for (Empleado empleado : empleados) {
-            double nuevoSalario = empleado.salario * (1 + porcentaje / 100);
-            empleado.salario = nuevoSalario;
-        }
-    }
-
-    public Empleado[] getEmpleados() {
-        return empleados;
-    }
-
-    public void setEmpleados(Empleado[] empleados) {
-        this.empleados = empleados;
-    }
-}
 
 /**
  * Esta clase representa un sistema de gestión de empleados.
